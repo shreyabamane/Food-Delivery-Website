f0592899ab7cf105b135c4436abad4dc16595e41
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-07-01 19:34:35 +0530

    create header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ export function Header() {
   return (
     <div className='header'>
       <div className="header-contents">
-        hello
+        <h2>Order Your Favourite Food Here!</h2>
+        <p>Are your taste buds craving a culinary adventure? Ready to indulge in some mouth-watering delights? You've come to the right place! At Craveful, we bring you a symphony of flavors that will tantalize your senses and leave you craving for more.</p>
+        <button>View Menu</button>
       </div>
     </div>
   )
