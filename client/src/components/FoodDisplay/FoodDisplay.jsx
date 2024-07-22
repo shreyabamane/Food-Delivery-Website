35cdf3731f67b6596ab01290eca822cf838b41e4
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-07-22 18:19:06 +0530

    add food display component

---
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FoodDisplay.css';
+
+export default function FoodDisplay() {
+  return (
+    <div className='food-display'>
+      <h2>Top dishes near you</h2>
+    </div>
+  )
+}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from '../../components/Header/Header'
 import { ExploreMenu } from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 export function Home() {
   return (
     <div>
       <Header/>
       <ExploreMenu />
+      <FoodDisplay />
     </div>
   )
 }
