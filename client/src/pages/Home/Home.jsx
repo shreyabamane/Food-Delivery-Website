d52619a47fd6cf3a02fe733ab2fa92136ae729c4
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-07-22 18:00:13 +0530

    add explore menu component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../components/Header/Header'
+import { ExploreMenu } from '../../components/ExploreMenu/ExploreMenu'
 
 export function Home() {
   return (
     <div>
       <Header/>
+      <ExploreMenu />
     </div>
   )
 }
