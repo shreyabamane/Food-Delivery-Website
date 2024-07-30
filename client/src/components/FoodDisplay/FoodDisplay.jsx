c6023c0e20519cd3a044f87da947727543c01eed
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-07-30 17:33:01 +0530

    add food item component

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './FoodDisplay.css';
 import { food_list } from '../../assets/assets';
+import { FoodItem } from '../FoodItem/FoodItem';
 
 export default function FoodDisplay() {
   return (
@@ -8,7 +9,14 @@ export default function FoodDisplay() {
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          
+          return <FoodItem 
+          key={index}
+          id={item._id}
+          name={item.name}
+          description={item.description}
+          price={item.price}
+          image={item.image}
+          />
         })}
       </div>
     </div>
---
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FoodItem.css';
+
+export function FoodItem({id,name,price,description,image}) {
+  return (
+    <div className='food-item'>
+      
+    </div>
+  )
+}
