d56a818bde8975e1960b8ffd870f834c53d9879c
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-07-30 15:45:32 +0530

    modify food display component

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './FoodDisplay.css';
+import { food_list } from '../../assets/assets';
 
 export default function FoodDisplay() {
   return (
     <div className='food-display'>
       <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          
+        })}
+      </div>
     </div>
   )
 }
