bc1594f5dac9a4a3efd53e767cb9f49b099d5853
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-02 16:41:36 +0530

    add category parameter in food display component

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,23 +3,25 @@ import './FoodDisplay.css';
 import { StoreContext } from '../../context/StoreContext';
 import { FoodItem } from '../FoodItem/FoodItem';
 
-export function FoodDisplay() {
+export function FoodDisplay({ category }) {
 
-  const {food_list} = useContext(StoreContext) 
+  const { food_list } = useContext(StoreContext)
 
   return (
     <div className='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return <FoodItem 
-          key={index}
-          id={item._id}
-          name={item.name}
-          description={item.description}
-          price={item.price}
-          image={item.image}
-          />
+          if (category === "All" || category === item.category) {
+            return <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          }
         })}
       </div>
     </div>
