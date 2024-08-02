7fc5bd4f7499a504e3c930b068e0dd077f56aa65
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-02 17:42:16 +0530

    add name,description,image,price in food item component

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './FoodItem.css';
+import { assets } from '../../assets/assets';
 
 export function FoodItem({id,name,price,description,image}) {
   return (
     <div className='food-item'>
+        <img className='food-item-image' src={image} alt="" />
       
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+            <p>{name}</p>
+            <img src={assets.rating_stars} alt="" />
+        </div>
+        <div className="food-item-desc">{description}</div>
+        <div className="food-item-price">₹{price}</div>
+      </div>
     </div>
   )
 }
