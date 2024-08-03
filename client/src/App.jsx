023b19835713c129d83977013a3e32d9f11b4b62
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-03 21:50:25 +0530

    add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Navbar } from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
+
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
