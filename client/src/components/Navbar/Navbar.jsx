68e3425c8ccc34f445d68976b5d568b324f9bd8a
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-11 19:24:47 +0530

    responsive navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { CiSearch } from "react-icons/ci";
 import { FaShoppingBasket } from "react-icons/fa";
 
 export function Navbar() {
+
+  const [menu, setMenu] = useState("menu");
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="logo" className='logo' />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-App</li>
-        <li>Contact</li>
+        <li onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-App</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</li>
       </ul>
 
       <div className="navbar-right">
@@ -20,9 +22,10 @@ export function Navbar() {
 
         <div className="navbar-search-icon">
           <FaShoppingBasket size={25} className='image' />
+          <div className="dot"></div>
         </div>
 
-      <button>sign in</button>
+        <button>sign in</button>
       </div>
     </div>
   )
