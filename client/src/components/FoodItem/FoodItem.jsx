a6f0ba883f91e69ac54885e64217eb088de9ed1e
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-12 13:20:23 +0530

    add button element

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -5,8 +5,10 @@ import { assets } from '../../assets/assets';
 export function FoodItem({id,name,price,description,image}) {
   return (
     <div className='food-item'>
+      <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="" />
-      
+        <button>ADD</button>
+        </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
             <p>{name}</p>
