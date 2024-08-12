cacef1d6d3825dea461071f88b6455069286dc00
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-12 16:29:51 +0530

    add itemCount in food item component

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FoodItem.css';
 import { assets } from '../../assets/assets';
+import { IoMdAddCircle, IoMdRemoveCircle } from "react-icons/io";
 
 export function FoodItem({id,name,price,description,image}) {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="" />
-        <button>ADD</button>
+       {
+        !itemCount
+        ?  <button className='add' onClick={()=>setItemCount(prev=>prev+1)}>ADD</button>
+        :<div className='food-item-counter'>
+           <IoMdRemoveCircle onClick={() => setItemCount(prev => prev-1)} color='red' size={30} />
+           <p>{itemCount}</p>
+           <IoMdAddCircle onClick={() => setItemCount(prev => prev+1)} color='green' size={30} />
+        </div>
+       }
         </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
