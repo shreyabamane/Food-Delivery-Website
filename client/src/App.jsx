33b137a3b3d411b6fa281e1554192815f4f18660
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-31 16:30:31 +0530

    create login-popup format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-    {showLogin ? <LoginPopup /> : <></>}
+    {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
--- client/src/components/LoginPopup/LoginPopup.jsx
+++ client/src/components/LoginPopup/LoginPopup.jsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPopup.css';
+import { RxCross1 } from "react-icons/rx";
 
-export function LoginPopup() {
+
+export function LoginPopup({ setShowLogin }) {
+
+  const [currState, setCurrState] = useState("Login");
+
   return (
     <div className='login-popup'>
       <form className="login-popup-container">
+
         <div className="login-popup-title">
-          <h2></h2>
+          <h2>{currState}</h2>
+          <RxCross1 onClick={() => setShowLogin(false)} size={20} />
         </div>
+
+        <div className="login-popup-inputs">
+          {currState === "Login" ? <></> : <input type="text" placeholder='Your Name' required />}
+          <input type="email" placeholder='Your Email' required />
+          <input type="password" placeholder='Password' required />
+        </div>
+
+        <button>{currState === 'Sign Up' ? "Create Account" : 'Login'}</button>
+
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+
+        {currState === "Login"
+          ? <p>Create a new account? <span onClick={()=> setCurrState("Sign Up")}>Click here</span></p>
+          : <p>Already have an account? <span onClick={()=> setCurrState("Login")}>Login here</span></p>
+        }
       </form>
     </div>
   )
