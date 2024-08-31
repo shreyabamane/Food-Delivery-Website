5e062bd93fe36f96a5cdbe986a30946a3dfbd0f2
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-31 15:56:23 +0530

    create cart page

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
+import { IoMdAddCircle, IoMdRemoveCircle } from "react-icons/io";
 
 export function Cart() {
+  const { food_list, cartItems, addToCart, removeFromCart } = useContext(StoreContext);
   return (
     <div className='cart'>
       <div className="cart-items">
-        <div className='cart-items-title'>
-            cart
-        </div>
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div className="cart-items-item">
+                <div className="cart-image">
+                  <img src={item.image} alt="" />
+                </div>
+                <div className="cart-info">
+                <div className='cart-item-counter'>
+                    <IoMdRemoveCircle onClick={() => removeFromCart(item._id)} color='red' size={30} />
+                    <p>{cartItems[item._id]}</p>
+                    <IoMdAddCircle onClick={() => addToCart(item._id)} color='green' size={30} />
+                  </div>
+                  <p style={{ color: 'grey', fontSize: '16px' }}>{item.name}</p>
+                  <p style={{ fontWeight: '500' }}>₹{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <h3 className='item-total-price'>Total: <p>₹{item.price * cartItems[item._id]}</p></h3>
+
+                </div>
+
+              </div>
+            )
+          }
+        })
+        }
       </div>
     </div>
   )
