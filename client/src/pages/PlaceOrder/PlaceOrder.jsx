da90a0cc47bcb92d0f8a3d389b5722b9f57a5137
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-09-02 20:49:54 +0530

    add content in left side of place order

--- client/src/pages/PlaceOrder/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './PlaceOrder.css';
 
 export function PlaceOrder() {
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+
+         <p className="title">Delivery Information</p>
+
+         <div className="multi-fields">
+            <input type="text" placeholder='First Name' />
+            <input type="text" placeholder='Last Name' />
+         </div>
+
+         <input type="email" placeholder='Email address' />
+         <input type="text" placeholder='Streer' />
+
+         <div className="multi-fields">
+            <input type="text" placeholder='City' />
+            <input type="text" placeholder='State' />
+         </div>
+
+         <div className="multi-fields">
+            <input type="text" placeholder='Zip Code' />
+            <input type="text" placeholder='Country' />
+         </div>
+
+         <input type="text" placeholder='Phone' />
+      </div>
+
+      <div className="place-order-right"></div>
+    </form>
   )
 }
