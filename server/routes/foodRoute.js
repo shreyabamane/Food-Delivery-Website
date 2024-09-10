fdb10ab97921cbb4d8c9a3ca215581539cf420b2
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-09-10 23:46:18 +0530

    create POST request to add a new food item

--- server/routes/foodRoute.js
+++ server/routes/foodRoute.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { addFood } from "../controllers/foodController.js";
 
 // Create a new router instance for handling food-related routes
 const foodRouter = express.Router();
+
+foodRouter.post("/add", addFood)
 
 
 export default foodRouter;
