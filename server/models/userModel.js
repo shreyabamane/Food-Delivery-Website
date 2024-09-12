caeb0b1b6892d68256f76cdca3b5585428f64049
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-09-12 18:30:58 +0530

    add properties that will be in user model.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true,unique:true},
-    password:{type:String,required:true},
-    cartData:{type:Object,default:{}} //manage users cart
-},{minimize:false}) //if we don't add this false in that case cart data will not created because we have not provided data here.
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cartData: { type: Object, default: {} } //manage users cart
+}, { minimize: false }) //if we don't add this false in that case cart data will not created because we have not provided data here.
 
-const userModel = mongoose.models.user || mongoose.model("user",userSchema);
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
 export default userModel;
