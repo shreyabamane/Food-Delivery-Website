4b31166dbb6fd2651b44117cd3e10026bedb8f81
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-09-12 15:39:08 +0530

    add endpoint for removing food items in database

--- server/routes/foodRoute.js
+++ server/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFood, listFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 // Create a new router instance for handling food-related routes
@@ -15,8 +15,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
-foodRouter.post("/add", upload.single("image"), addFood)
-foodRouter.get("/list", listFood)
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
