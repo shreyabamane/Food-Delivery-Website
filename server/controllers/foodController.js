e148debd07b2008def5e2b12a912ddde62ce354a
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-09-12 15:20:52 +0530

    add try-catch block in listfood

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -23,7 +23,13 @@ const addFood = async (req, res) => {
 
 
 const listFood = async (req, res) => {
-
+    try {
+        const foods = await foodModel.find({});
+        res.json({ success: true, data: foods });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" });
+    }
 }
 
 export { addFood, listFood }
