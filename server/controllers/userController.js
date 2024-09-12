9f09e0ee89fec246b9283a7dfb5834c0436b48be
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-09-12 19:16:36 +0530

    Check if a user with the given email already exists in the database.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+import userModel from "../models/userModel.js";
 
 
 const loginUser = async (req, res) => {
@@ -5,7 +6,17 @@ const loginUser = async (req, res) => {
 }
 
 const registerUser = async (req, res) => {
-
+    const { name, password, email } = req.body;
+    try {
+        //Check if a user with the given email already exists in the database
+        const exists = await userModel.findOne({ email })
+        if (exists) {
+            return res.json({ success: false, message: "User already exists" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" });
+    }
 }
 
 export { loginUser, registerUser }
